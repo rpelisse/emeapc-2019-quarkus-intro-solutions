e4c0b0b810b807385ee8dc077b8f97cd7d479cc7
Romain Pelisse <belaran@gmail.com>
2019-06-20 12:10:25 +0200

    Lab 2 - Using the ReST Client

--- src/main/java/org/redhat/emeapc/GreetingResource.java
+++ src/main/java/org/redhat/emeapc/GreetingResource.java
@@ -1,21 +1,20 @@
 package org.redhat.emeapc;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.logging.Logger;
 
 import javax.inject.Inject;
-import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
 import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @Path("/quarkus")
 @Produces(MediaType.APPLICATION_JSON)
@@ -23,33 +22,39 @@ public class GreetingResource {
 
     private static final Logger LOGGER = Logger.getLogger(GreetingResource.class.getName());
 
-    private static final List<Answer> answers = new ArrayList<>(1);
+    private static final Map<String,Country> cacheCountries = new HashMap<String,Country>();
 
+    @Inject
+    @RestClient
+    CountriesService countriesService;
+    
     @Inject
     Validator validator;
 
-    public GreetingResource() {
-        answers.add(new Answer("hello", "a typical answer"));
-    }
-
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Answer> hello() {
-        LOGGER.warning("Hello was invoked().");
-        return answers;
+    public Collection<Country> cachedCountries() {
+        LOGGER.warning("cachedCountries() was invoked.");        
+        return cacheCountries.values();
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/country/{countryName}")
+    public Country retrieveCountryByName(@PathParam("countryName") String countryName ) {
+        LOGGER.warning("retrieveCountryByName was invoked.");        
+    	return cacheCountries.containsKey(countryName) ? cacheCountries.get(countryName) : invokedRemoteService(countryName);
     }
 
-    @PUT
-    @Path("/{name}/{description}")
-    public void addAnswer(@PathParam(value = "name") String name,@PathParam(value = "description") String description) {
-        LOGGER.warning("add Answer was invoked() with:" + name + "/" + description);
-        Answer newAnswer = new Answer(name, description);
-        Set<ConstraintViolation<Answer>> violations = validator.validate(newAnswer);
-        if ( ! violations.isEmpty() )
-            throw new IllegalArgumentException(violations.toString());
-        answers.clear();
-        LOGGER.warning("answers list cleared.");
-        answers.add(newAnswer);
-        LOGGER.warning("answers list updated.");
-    }
+	private Country invokedRemoteService(String countryName) {
+		Set<Country> res = countriesService.getByName(countryName);
+		if ( res == null || res.isEmpty() )
+			throw new IllegalArgumentException("No such country :" + countryName);
+		return cacheCountry(countryName, res.iterator().next());
+	}
+	
+	private Country cacheCountry(String countryName, Country country) {
+		cacheCountries.put(countryName, country);
+		return country;
+	}
 }
